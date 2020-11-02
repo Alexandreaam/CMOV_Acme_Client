b4c1cfe42785b4d55974438f8f5bdb7cebfb5b42
Alexandre <alex.al.mar@hotmail.com>
2020-11-02 11:04:41 +0000

    Login register error checking

--- app/src/main/java/com/acme_industries/acmecaf/RegistryPage.kt
+++ app/src/main/java/com/acme_industries/acmecaf/RegistryPage.kt
@@ -1,17 +1,20 @@
 package com.acme_industries.acmecaf
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import kotlinx.android.synthetic.main.fragment_register_data.*
+import kotlinx.android.synthetic.main.fragment_simple_login.*
 
 class RegistryPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide();
+        supportActionBar?.hide()
         setContentView(R.layout.activity_registry_page)
-        fillFields();
+        fillFields()
     }
 
     private fun fillFields() {
@@ -26,11 +29,49 @@ class RegistryPage : AppCompatActivity() {
     fun loginFunction(view: View) {
         val username = findViewById<EditText>(R.id.editTextUser2).text.toString()
         val password = findViewById<EditText>(R.id.editTextPass2).text.toString()
+        val passwordConfirm = findViewById<EditText>(R.id.editTextPassCheck).text.toString()
+        val realName = findViewById<EditText>(R.id.editTextRealName).text.toString()
+        val creditDebit = findViewById<EditText>(R.id.editTextCreditDebit).text.toString()
+        val NIF = findViewById<EditText>(R.id.editTextNIF).text.toString()
 
-        val intent = Intent(this, MainActivityPage::class.java).apply {
-            putExtra("user", username)
-            putExtra("pass", password)
+        when {
+            username == "" -> {
+                editTextUser2.setBackgroundResource(R.drawable.edit_text_error)
+            }
+            password == "" -> {
+                editTextPass2.setBackgroundResource(R.drawable.edit_text_error)
+            }
+            passwordConfirm == "" -> {
+                editTextPassCheck.setBackgroundResource(R.drawable.edit_text_error)
+            }
+            realName == "" -> {
+                editTextRealName.setBackgroundResource(R.drawable.edit_text_error)
+            }
+            creditDebit == "" -> {
+                editTextCreditDebit.setBackgroundResource(R.drawable.edit_text_error)
+            }
+            NIF == "" -> {
+                editTextNIF.setBackgroundResource(R.drawable.edit_text_error)
+            }
+            password != passwordConfirm -> {
+                pass_warning.visibility = View.VISIBLE
+                editTextPass2.setBackgroundResource(R.drawable.edit_text_error)
+                editTextPassCheck.setBackgroundResource(R.drawable.edit_text_error)
+                editTextPass2.setTextColor(Color.parseColor("#FF0000"))
+                editTextPassCheck.setTextColor(Color.parseColor("#FF0000"))
+
+            }
+            else -> {
+                val intent = Intent(this, MainActivityPage::class.java).apply {
+                    putExtra("user", username)
+                    putExtra("pass", password)
+                }
+                startActivity(intent)
+            }
         }
-        startActivity(intent)
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/acme_industries/acmecaf/core/Client.kt
@@ -0,0 +1,4 @@
+package com.acme_industries.acmecaf.core
+
+data class Client (val username: String, val password: String, val fullName: String, val creditCard: Int, val NIF: String) {
+}
