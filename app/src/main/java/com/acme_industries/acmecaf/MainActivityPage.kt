dcbdda2d4169bc49d57818a19451eb854d1ef1ec
Alexandreaam <alex.al.mar@hotmail.com>
2020-11-15 22:45:06 +0000

    Checkout button now appears in every page and is only visible when items are selected

--- app/src/main/java/com/acme_industries/acmecaf/MainActivityPage.kt
+++ app/src/main/java/com/acme_industries/acmecaf/MainActivityPage.kt
@@ -1,18 +1,25 @@
 package com.acme_industries.acmecaf
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.acme_industries.acmecaf.core.Constants
 import com.acme_industries.acmecaf.core.MainViewModel
+import com.acme_industries.acmecaf.core.QRBuilder
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivityPage : AppCompatActivity() {
 
@@ -39,6 +46,15 @@ class MainActivityPage : AppCompatActivity() {
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        findViewById<Button>(R.id.checkout_button).setOnClickListener { checkout() }
+
+        cartModel.itemsLiveData.observe(this) {
+            if(cartModel.cart.orderList.isEmpty())
+                findViewById<Button>(R.id.checkout_button).visibility = View.INVISIBLE
+            else
+                findViewById<Button>(R.id.checkout_button).visibility = View.VISIBLE
+        }
+
         val navController = findNavController(R.id.nav_host_fragment)
 
         val appBarConfiguration = AppBarConfiguration(
@@ -49,5 +65,28 @@ class MainActivityPage : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun checkout() {
+        val modalSheetView = layoutInflater.inflate(R.layout.bottom_layout_sheet,null)
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(modalSheetView)
+
+        val list = modalSheetView.findViewById<TextView>(R.id.checkout_resume)
+        val total = modalSheetView.findViewById<TextView>(R.id.checkout_total)
+        var temp = ""
+
+        cartModel.cart.orderList.map { temp += (it.product.title + " x " + it.quantity + "\n")}.toString()
+        list.text = temp
+        total.text = "Total:${"%.2f€".format(cartModel.cart.totalCost)}"
+
+        //TODO Add user key
+        val qr = QRBuilder(this)
+        val bitmap = qr.encodeAsBitmap(cartModel.getFormatedData())
+        val image = modalSheetView.findViewById<ImageView>(R.id.qr_image)
+        image.setImageBitmap(bitmap)
+
+
+        dialog.show()
     }
 }
--- app/src/main/java/com/acme_industries/acmecaf/ui/home/HomeFragment.kt
+++ app/src/main/java/com/acme_industries/acmecaf/ui/home/HomeFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.observe
@@ -13,9 +11,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.acme_industries.acmecaf.R
 import com.acme_industries.acmecaf.core.MainViewModel
-import com.acme_industries.acmecaf.core.QRBuilder
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -45,34 +40,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val checkoutButton: ExtendedFloatingActionButton = root.findViewById(R.id.checkout_button)
-        checkoutButton.setOnClickListener {
-            checkout(root)
-        }
-        return root
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    private fun checkout(root: View) {
-        val modalSheetView = layoutInflater.inflate(R.layout.bottom_layout_sheet,null)
-        val dialog = BottomSheetDialog(root.context)
-        dialog.setContentView(modalSheetView)
 
-        val list = modalSheetView.findViewById<TextView>(R.id.checkout_resume)
-        val total = modalSheetView.findViewById<TextView>(R.id.checkout_total)
-        var temp = ""
-        cartModel.cart.orderList.map { temp += (it.product.title + " x " + it.quantity + "\n")}.toString()
-        list.text = temp
-        total.text = "Total:${"%.2f€".format(cartModel.cart.totalCost)}"
-        //TODO Add user key
-        val qr = QRBuilder(root.context)
-        val bitmap = qr.encodeAsBitmap(cartModel.getFormatedData())
-        val image = modalSheetView.findViewById<ImageView>(R.id.qr_image)
-        image.setImageBitmap(bitmap)
-
-
-        dialog.show()
-
-
-    }
 }
