20f566a44ac6b406635464d706f63b08f400e57a
Alexandre <alex.al.mar@hotmail.com>
2020-11-21 23:17:45 +0000

    Revert

--- app/src/main/java/com/acme_industries/acmecaf/MainActivityPage.kt
+++ app/src/main/java/com/acme_industries/acmecaf/MainActivityPage.kt
@@ -138,7 +138,5 @@ class MainActivityPage : AppCompatActivity() {
 
         findViewById<Button>(R.id.checkout_button).visibility = View.INVISIBLE
         findViewById<FloatingActionButton>(R.id.refresh_button).visibility = View.VISIBLE
-        cartModel.reset()
-        reload()
     }
 }
